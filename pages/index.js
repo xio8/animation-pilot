e381277d8c86d00160c359e025a2351c42d0be6e
shankar-at-724493240358 <shankar@syscloud.org>
2021-08-30 12:53:49 +0530

    removed framer package

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,67 @@
-/* eslint-disable @next/next/no-img-element */
-import { motion } from "framer-motion";
-import { useState } from "react";
-export default function App({
-  data = [
-    { x: 130, y: -130, scale: 1.3 },
-    { x: -100, y: -100, scale: 0.9 },
-    { x: 0, y: 0, scale: 0.7 },
-    { x: 100, y: 100, scale: 0.6 },
-    { x: -100, y: 100, scale: 0.8 },
-  ],
-}) {
-  const [s, ss] = useState(data);
-  const [t, st] = useState(1);
+import { makeStyles } from "@material-ui/core/styles";
+
+import { useState, useEffect} from "react";
+
+const useStyles = makeStyles((theme) => ({
+  c0: { transform: "translate(130px,-130px)", transition: "transform 2s" },
+  c1: { transform: "translate(-100px, -100px)", transition: "transform 2s" },
+  c2: { transform: "translate(0px, 0px)", transition: "transform 2s" },
+  c3: { transform: "translate(100px, 100px)", transition: "transform 2s" },
+  c4: { transform: "translate(-100px, 100px)", transition: "transform 2s" }
+}));
+export default function App({}) {
+  const classes = useStyles();
+
+  const [s, ss] = useState([
+    classes.c0,
+    classes.c1,
+    classes.c2,
+    classes.c3,
+    classes.c4
+  ]);
+  const [a, sa] = useState(0);
+
+  function change(i) {
+    sa(i);
+  }
+
+  useEffect(() => {
+    let temp = [classes.c0, classes.c1, classes.c2, classes.c3, classes.c4];
+    let t;
+    t = temp[0];
+    temp[0] = temp[a];
+    temp[a] = t;
+    console.log(a);
+
+    ss(temp);
+  }, [a]);
 
   return (
-    <div className="App" style={{ position: "relative", padding: 200 }}>
-      {s.map((x, i) => {
-        return (
-          <motion.span
-            key={x}
-            transition={{ duration: t }}
-            style={{ position: "absolute" }}
-            animate={{ ...x }}
-            onClick={() => {
-              let temp = data;
-              let t;
-              st(2);
-              t = temp[0];
-              temp[0] = temp[i];
-              temp[i] = t;
-              console.log(temp);
-              ss(temp);
-            }}
-          >
-            {i}
-            <img
-              height="100"
-              src="https://uploads.codesandbox.io/uploads/user/9c749c9c-2fd3-4110-a4d4-b71b9325153c/hc5G-user.png"
-              alt="cc"
-            />
-          </motion.span>
-        );
-      })}
-    </div>
+    <>
+      <div className="App" style={{ position: "relative", padding: 200 }}>
+        {[0, 1, 2, 3, 4].map((x, i) => {
+          return (
+            <div
+              key={i * i}
+              id={i}
+              style={{ position: "absolute" }}
+              className={s[i]}
+              onClick={() => {
+                change(i);
+              }}
+            >
+              {i}
+              <img
+                height="100"
+                src={
+                  "https://uploads.codesandbox.io/uploads/user/2559df5f-382a-4254-8815-3d91b4ca89b2/Anww-user.png"
+                }
+                alt="12"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
-
-export async function getStaticProps(context) {
-  const s = { ss: "s" };
-  return {
-    props: { s }, // will be passed to the page component as props
-  };
 }
